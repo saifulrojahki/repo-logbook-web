bf44b1d8693cbf1db17103c53432d3b23a7484a9
saifulrojahki <s.rojahki@gmail.com>
2020-02-16 21:21:01 +0700

    perbaiki error tampilan profile

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -13,7 +13,7 @@
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row no-gutters">
             <div class="col-md-4">
-                <img src="<?= base_url('assets/img/profile/') . $image['image']; ?>" class="card-img" alt="...">
+                <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="card-img" alt="...">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
