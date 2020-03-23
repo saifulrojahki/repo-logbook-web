fc33ff82bd52f896856ec8770c500229c8b3fe2c
saifulrojahki <s.rojahki@gmail.com>
2020-03-23 15:42:27 +0700

    merubah layout tampilan foto sae best performance di ranking

--- application/views/competition/ranking.php
+++ application/views/competition/ranking.php
@@ -8,55 +8,74 @@
     <div class="row">
         <div class="col-lg">
             <label for="formGroupExampleInput"><?= $this->session->userdata('code_competition'); ?></label>
+            <!-- set grid system -->
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-10">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-sm">
+                                <thead>
+                                    <tr class="table-primary">
+                                        <th scope="col" rowspan="2">#</th>
+                                        <th scope="col" rowspan="2">MIP</th>
+                                        <th scope="col" rowspan="2">Name</th>
+                                        <th scope="col" colspan="2">
+                                            <center>Productivity</center>
+                                        </th>
+                                        <th scope="col" colspan="2">
+                                            <center>PM Complete</center>
+                                        </th>
+                                        <th scope="col" rowspan="2">Absent</th>
+                                        <th scope="col" rowspan="2">SLA</th>
+                                        <th scope="col" rowspan="2">Reward</th>
+                                        <th scope="col" rowspan="2">Pinalty</th>
+                                        <th scope="col" rowspan="2">Grand Total</th>
+                                    </tr>
+                                    <tr class="table-primary">
+                                        <th scope="col">Point</th>
+                                        <th scope="col">Score</th>
+                                        <th scope="col">Date</th>
+                                        <th scope="col">Score</th>
 
-            <div class="table-responsive">
-                <table class="table table-bordered table-sm">
-                    <thead>
-                        <tr class="table-primary">
-                            <th scope="col" rowspan="2">#</th>
-                            <th scope="col" rowspan="2">MIP</th>
-                            <th scope="col" rowspan="2">Name</th>
-                            <th scope="col" colspan="2">
-                                <center>Productivity</center>
-                            </th>
-                            <th scope="col" colspan="2">
-                                <center>PM Complete</center>
-                            </th>
-                            <th scope="col" rowspan="2">Absent</th>
-                            <th scope="col" rowspan="2">SLA</th>
-                            <th scope="col" rowspan="2">Reward</th>
-                            <th scope="col" rowspan="2">Pinalty</th>
-                            <th scope="col" rowspan="2">Grand Total</th>
-                        </tr>
-                        <tr class="table-primary">
-                            <th scope="col">Point</th>
-                            <th scope="col">Score</th>
-                            <th scope="col">Date</th>
-                            <th scope="col">Score</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php $i = 1; ?>
+                                    <?php foreach ($competition as $sm) : ?>
+                                        <tr>
+                                            <th scope="row"><?= $i; ?></th>
+                                            <td><?= $sm['mip']; ?></td>
+                                            <td><?= $sm['nama']; ?></td>
+                                            <td><?= $sm['productivity']; ?></td>
+                                            <td><?= $sm['bobot_productivity']; ?></td>
+                                            <td><?= $sm['pm']; ?></td>
+                                            <td><?= $sm['bobot_pm']; ?></td>
+                                            <td><?= $sm['bobot_absen']; ?></td>
+                                            <td><?= $sm['mis_sla']; ?></td>
+                                            <td><?= $sm['reward']; ?></td>
+                                            <td><?= $sm['pinalty']; ?></td>
+                                            <td><?= $sm['total'];; ?></td>
+                                        </tr>
+                                        <?php $i++; ?>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $i = 1; ?>
-                        <?php foreach ($competition as $sm) : ?>
-                            <tr>
-                                <th scope="row"><?= $i; ?></th>
-                                <td><?= $sm['mip']; ?></td>
-                                <td><?= $sm['nama']; ?></td>
-                                <td><?= $sm['productivity']; ?></td>
-                                <td><?= $sm['bobot_productivity']; ?></td>
-                                <td><?= $sm['pm']; ?></td>
-                                <td><?= $sm['bobot_pm']; ?></td>
-                                <td><?= $sm['bobot_absen']; ?></td>
-                                <td><?= $sm['mis_sla']; ?></td>
-                                <td><?= $sm['reward']; ?></td>
-                                <td><?= $sm['pinalty']; ?></td>
-                                <td><?= $sm['total'];; ?></td>
-                            </tr>
-                            <?php $i++; ?>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+                    <div class="col-lg-2">
+                        <!-- tampil gambar pemenang -->
+                        <div class="jumbotron jumbotron-fluid">
+                            <div class="container text-center">
+                                <img src="<?= base_url('assets/img/profile/') . $gambar['image']; ?>" width="100" class="figure-img img-fluid rounded-circle">
+                                <p class="lead"><?= $gambar['nama']; ?></p>
+                                <h6 class="display-8">Best Performance</h6>
+                                <h8 class="display-8"><?= $gambar['code_competition']; ?></h8>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
 
@@ -65,15 +84,7 @@
 </div>
 <!-- /.container-fluid -->
 
-<!-- tampil gambar pemenang -->
-<div class="jumbotron jumbotron-fluid">
-    <div class="container text-center">
-        <img src="<?= base_url('assets/img/profile/') . $gambar['image']; ?>" width="150" class="figure-img img-fluid rounded-circle">
-        <p class="lead"><?= $gambar['nama']; ?></p>
-        <h1 class="display-8">Best Performance</h1>
-        <h7 class="display-8"><?= $gambar['code_competition']; ?></h7>
-    </div>
-</div>
+
 
 </div>
 <!-- End of Main Content -->
